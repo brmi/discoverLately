1b7e5d127c5cbdfa6d618242bd1ed543c25d3d4a
brmi <desireelenart@gmail.com>
2018-01-19 23:41:04 -0800

    got top tracks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 let fakeServerData = {
   user: {
-    name: 'Desiree',
+    name: '{{display_name}}',
     playlists: [
       {
         name: 'Weewo',
@@ -36,6 +36,8 @@ let fakeServerData = {
     ]
   }
 };
+
+let playlistServerData = {};
 
 class PlaylistCounter extends Component {
   render() {
@@ -77,49 +79,164 @@ class SearchBar extends Component{
 
 class Playlist extends Component{
   render() {
-    let playlist = this.props.playlist
+    let playlist = this.props.playlist;
     return(
       <div>
         <img />
         <h3>{playlist.name}</h3>
-        <ul>
-          {
-            playlist.songs.map(song => 
-              <li> {song.name} </li>
-          )}
-        </ul>
+        <h4>{playlist.tracks.total}</h4>
+        <img src={playlist.images[0].url}/>
+        {
+        //   <ul>
+        //   {
+        //     playlist.songs.map(song => 
+        //       <li> {song.name} </li>
+        //   )}
+        // </ul>
+        }
       </div>
     )
+  }
+}
+
+class TopTracks extends Component {
+  render(){
+    let tracks = this.props.tracks;
+    return(
+      <div>
+        <h3>{tracks.name}</h3>
+        <ul>
+          {
+            tracks.artists.map(artist =>
+              <li>{artist.name}</li>)
+          }
+        </ul>
+      </div>
+      )
   }
 }
 
 class App extends Component {
   constructor(){
     super();
-    this.state = {serverData: {},
-    filterString: ''
+    this.state = {
+    userData: {
+      user_id: 0,
+      followers: 0,
+      href: '',
+      profilePic: ''
+    },
+    filterString: '',
+    artist: null,
+    playlistServerData: {},
+    topTracksData: {},
+    accessToken: 'BQBFq4YUQORQEAMVtec0ZEWo7MZlxetrCfPmkLMVGY7WjwF7SEI0kxwGPVuuKYcW9ofHHSSjmZWJcyqaCgZpQ2qFh9ncdKWpWVhNAHJNDML2TXMUdxZD6iF39D79dNfY4kxoY_AsKeI0RlyM8WV1eMNDw-O0D6jlsZCsbXA'
     }
   }
   componentDidMount() {
-    setTimeout( () => {
-      this.setState({serverData: fakeServerData});
-    }, 1000);
+    this.getUserData();
+    this.getPlaylistData();
+    this.getTopTracksData();
+  }
+
+  getPlaylistData() {
+    const LIMIT = 50;
+    const BASE_URL = 'https://api.spotify.com/v1/';
+    // const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    const FETCH_URL = BASE_URL + 'me/playlists?limit=' + LIMIT ;
+    var accessToken = this.state.accessToken;
+
+    var myOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    fetch(FETCH_URL, myOptions)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({playlistServerData: json});
+        console.log('get playlist data: ', this.state.playlistServerData);
+      })
+  }
+
+  getTopTracksData() {
+    const TYPE = 'tracks';
+    const LIMIT = 20;
+    const BASE_URL = 'https://api.spotify.com/v1';
+    const FETCH_URL = BASE_URL + '/me/top/' + TYPE + '?limit=' + LIMIT+ '&offset=0';
+    // const FETCH_URL = 'https://api.spotify.com/v1/me/top/tracks?limit=20&offset=0';
+    var accessToken = this.state.accessToken;
+
+    var myOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    fetch(FETCH_URL, myOptions)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({topTracksData: json});
+        console.log('get top tracks data: ', this.state.topTracksData);
+      })
+  }
+
+  getUserData() {
+    const BASE_URL = 'https://api.spotify.com/v1/';
+    // const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    const FETCH_URL = BASE_URL + 'me';
+    var accessToken = this.state.accessToken;
+
+    var myOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    fetch(FETCH_URL, myOptions)
+      .then(response => response.json())
+      .then(json => { 
+        this.setState(
+          { userData: {
+            user_id: json.id,
+            followers: json.followers.total,
+            profilePic: json.images[0].url
+          }, 
+        });
+        console.log('get user data: ', this.state.userData);
+      });
   }
 
   render() {
     return (
       <div className="App">
-      {this.state.serverData.user ?
+      {this.state.playlistServerData.items && this.state.userData && this.state.topTracksData.items ?
         <div>
-        <h1> {this.state.serverData.user.name}'s Playlists </h1>
+        <h1> Music Resume </h1>
+        <img src={this.state.userData.profilePic}/ >
         <div className="numText">
-          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-          <HoursCounter playlists={this.state.serverData.user.playlists}/>
+          <PlaylistCounter playlists={this.state.playlistServerData.items}/>
+        </div>
+        <div className='topTracks'>
+          {
+            this.state.topTracksData.items.map(tracks => 
+              <TopTracks tracks={tracks}/>)
+          }
         </div>
         <SearchBar onTextChange={text => this.setState({filterString : text})}/>
         <div className="playlists">
         {
-          this.state.serverData.user.playlists.filter(playlist =>
+          this.state.playlistServerData.items.filter(playlist =>
               playlist.name.toLowerCase().includes(
                 this.state.filterString.toLowerCase())
             ).map(playlist => 
